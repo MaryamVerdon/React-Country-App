d8cef40b61346c9aeb31107ce40d7b1deb7dcc9f
Maryam Verdon <maryamverdon@gmail.com>
2022-09-27 14:34:07 +0200

    ajout app Country

---
+++ src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio earum
+        dolorum, nemo nam ut doloribus! Animi aliquid rerum perferendis
+        dignissimos quisquam quam dolor rem repudiandae. Quos expedita aperiam
+        ipsa necessitatibus. Molestiae similique labore odio illum sed vel
+        exercitationem mollitia quo accusamus voluptas cum, minus debitis modi
+        iste. Adipisci impedit deleniti sapiente eos omnis repudiandae,
+        aspernatur tempore dolore iure, excepturi officia sequi voluptates
+        voluptatibus! Ut, exercitationem. Quae reiciendis aperiam accusantium
+        eaque natus? Obcaecati sed quam tenetur in quos molestiae quidem sunt
+        aliquid eaque. Sit aspernatur reiciendis atque minus animi cum nam
+        autem, earum totam pariatur maxime corporis quibusdam neque? Laborum,
+        perferendis.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur eaque
+        quibusdam perferendis sint quo necessitatibus perspiciatis nisi. Quia
+        possimus explicabo accusantium odit dignissimos distinctio. Tempora,
+        incidunt. Beatae sequi dolores dolorem cupiditate odio autem accusantium
+        cum doloribus ullam quia aliquid commodi, accusamus, voluptate impedit
+        rerum iusto, corporis explicabo tempore. Iusto beatae sequi quibusdam,
+        ipsa qui magni sint nihil vero aspernatur nemo doloribus voluptate
+        similique incidunt commodi perspiciatis sunt aut temporibus corporis eos
+        esse. Molestias, nostrum nobis repudiandae itaque explicabo maxime et
+        optio expedita fugiat laborum perspiciatis sint, quaerat deleniti
+        voluptates modi aliquid, neque consequuntur vel fugit numquam velit
+        praesentium temporibus quae?
+      </p>
+    </div>
+  );
+};
+
+export default About;
