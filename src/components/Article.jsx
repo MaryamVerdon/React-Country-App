d8aa9f0860c5ed9d85e99d51b02c25a6a81afe7b
Maryam Verdon <maryamverdon@gmail.com>
2022-09-27 16:21:55 +0200

    update article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Article = ({article}) => {
+    const [isEdit, setIsEdit] = useState(false);
+    const [editContent, setEditContent] = useState("");
     const dateFormateur = (date) => {
         let newDate = new Date(date).toLocaleDateString("fr-FR", {
             year: "numeric",
@@ -13,15 +16,32 @@ const Article = ({article}) => {
         return newDate;
     };
 
+    const handleEdit = () => {
+        const data = {
+            author: article.author,
+            content: editContent ? editContent : article.content,
+            date: article.date,
+            updateDate: Date.now(),
+        };
+        axios
+            .put("http://localhost:3001/articles/" + article.id, data)
+            .then(() => setIsEdit(false));
+    };
+
     return (
-        <div className="article">
+        <div className="article" style={{ background: isEdit ? "#f3feff" : "white"}}>
             <div className="card-header">
                 <h3>{article.author}</h3>
                 <em>Posté le {dateFormateur(article.date)}</em>
             </div>
-            <p>{article.content}</p>
+            {
+                isEdit ? <textarea defaultValue={editContent ? editContent : article.content} autoFocus onChange={(e)=> setEditContent(e.target.value)}></textarea> :  <p>{editContent ? editContent : article.content}</p>
+            }
+           
             <div className="btn-container">
-                <button>Editer</button>
+                {
+                    isEdit ? <button onClick={()=> handleEdit()}>Valider</button> : <button onClick={() => setIsEdit(true)}>Editer</button>
+                }
                 <button>Supprimer</button>
             </div>
         </div>
