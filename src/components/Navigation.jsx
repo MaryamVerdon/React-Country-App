f63be7e027b11f355012cf1721da7b545e117e05
Maryam Verdon <maryamverdon@gmail.com>
2022-09-27 14:51:36 +0200

    add component Blog + style Blog & article

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,6 +11,9 @@ const Navigation = () => {
                 <NavLink to="/about" end  className={(nav) => (nav.isActive ? "nav-active" : " ")}>
                     <li>A propos</li>
                 </NavLink>
+                <NavLink to="/blog" end  className={(nav) => (nav.isActive ? "nav-active" : " ")}>
+                    <li>Blog</li>
+                </NavLink>
                
             </ul>
         </div>
