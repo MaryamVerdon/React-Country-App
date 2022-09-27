5cfb80d449954dc399f47bda20a3d4c79a3e253f
Maryam Verdon <maryamverdon@gmail.com>
2022-09-27 15:05:06 +0200

    gestion du nombre de caractères lors de l'envoie d'un msg

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const MIN_CHAR = 140;
+  const [error, setError] = useState(false);
+  const [content, setContent] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (content.length < MIN_CHAR) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>Blog</h1>
 
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" placeholder="Nom" />
-        <textarea placeholder="Message"></textarea>
+        <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafp" }}
+          placeholder="Message"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        {error && <p>Veuillez écrire un minimum de 140 caractères.</p>}
         <input type="submit" value="Envoyer" />
       </form>
 
