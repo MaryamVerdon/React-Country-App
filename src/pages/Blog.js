1470886d84b1abfef51830ce9caf773371f6091c
Maryam Verdon <maryamverdon@gmail.com>
2022-09-27 15:56:53 +0200

    ajout d'un article en BDD

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Article from "../components/Article";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
   const MIN_CHAR = 140;
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [error, setError] = useState(false);
   const [content, setContent] = useState("");
-  const handleSubmit = (e) => {
+
+  //Récupération des données
+  const getData = () => {
+    axios
+      .get("http://localhost:3001/articles")
+      .then((res) => setBlogData(res.data));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (content.length < MIN_CHAR) {
       setError(true);
     } else {
+      await axios.post("http://localhost:3001/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
 
@@ -22,17 +46,29 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
           style={{ border: error ? "1px solid red" : "1px solid #61dafp" }}
           placeholder="Message"
           onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un minimum de 140 caractères.</p>}
         <input type="submit" value="Envoyer" />
       </form>
 
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
